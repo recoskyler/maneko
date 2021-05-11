288e10faa2a6dd4172b09425ece15380df7de7b4
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2021-05-11 22:16:19 +0300

    Added OpenAPI specification validator Changed route name

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // Requirements
 
-const path    = require("path");
-const express = require("express");
-const session = require("express-session");
-const layouts = require("express-ejs-layouts");
+const path         = require("path");
+const express      = require("express");
+const session      = require("express-session");
+const layouts      = require("express-ejs-layouts");
+const apiValidator = require("express-openapi-validator");
 
 // Routers
 
 const indexRouter    = require("./routes/index");
 const loginRouter    = require("./routes/login");
 const mangaRouter    = require("./routes/manga");
-const authorRouter   = require("./routes/author");
 const searchRouter   = require("./routes/search");
 const profileRouter  = require("./routes/profile");
+const authorsRouter  = require("./routes/authors");
 const registerRouter = require("./routes/register");
 
 // Constants
@@ -45,14 +46,26 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+app.use(apiValidator.middleware({
+    apiSpec: './api.yaml',
+    validateRequests: true,
+    validateResponses: true
+}));
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+    });
+});
 
 // Routes
 
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/manga", mangaRouter);
-app.use("/author", authorRouter);
 app.use("/search", searchRouter);
+app.use("/authors", authorsRouter);
 app.use("/profile", profileRouter);
 app.use("/register", registerRouter);
 
