ccbc0412024bb23a9cbee722d6ed4ac5b2ae0bce
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2021-05-13 05:58:46 +0300

    Started to work on login

--- routes/login.js
+++ routes/login.js
@@ -2,11 +2,35 @@
 
 const express = require("express");
 const router  = express.Router();
+const { isDefined } = require("../toolbox");
 
 /////
 
+const BASE_URL = "https://api.mangadex.org";
+
 router.get("/", (req, res) => {
-    res.send("MangaRex is alive! LOGIN");
+    res.render("login");
+});
+
+router.post("/", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    axios.post("/auth/login", {
+        baseURL: BASE_URL,
+        params: {
+            username: username,
+            password: password
+        }
+    }).then((response) => {
+        
+
+
+        res.render("index");
+    }).catch((err) => {
+        console.error(err);
+        res.render("index");
+    });
 });
 
 module.exports = router;
