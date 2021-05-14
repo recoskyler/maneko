57212da72622e12892f775ded9e42c2ba9060464
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2021-05-14 04:21:00 +0300

    Completed login flow

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,14 @@
 // Requirements
 
+const toolbox = require("../toolbox");
 const express = require("express");
 const router  = express.Router();
 const axios   = require("axios").default;
 
 /////
 
-const BASE_URL = "https://api.mangadex.org";
-
 function login(req, res, username, password) {
-    axios.post(BASE_URL + "/auth/login", {
+    axios.post(`${toolbox.BASE_URL}/auth/login`, {
         username: username,
         password: password
     }).then((response) => {
@@ -20,55 +19,32 @@ function login(req, res, username, password) {
             console.log("Logged In");
             console.log()
 
-            res.redirect("/");
-        } else {
-            res.render("login", { error: "Can't login" });
+            return res.redirect("/");
         }
-    }).catch((err) => {
-        if (err.response.status === 401) {
-            console.error(err?.response?.headers["www-authenticate"]);
-            res.render("login", { error: "Wrong username or password" });
-        } else {
-            console.error(err?.response);
-            res.render("login", { error: "An error occurred" });
-        }
-    });
-}
-
-function checkLoggedIn(req, res) {
-    console.log("Checking if already logged in ...");
-
-    axios.get(BASE_URL + "/auth/check", {
-        baseURL: BASE_URL,
-        headers: {
-            Authorization: `Bearer ${req.session.session}`
-        }
-    }).then((response) => {
-        console.log("Response: " + response.data);
-
-        if (response.data.ok === "ok" && response.data.isAuthenticated) {
-            res.redirect("/profile");
-        }
+        
+        res.render("login", { error: "Can't login" });
     }).catch((err) => {
         console.error(err);
-        res.render("login", { error: err });
+        res.render("login", { error: "An error occurred" });
     });
 }
 
-router.get("/", (req, res) => {
-    if (typeof req.session.session === "string") {
-        checkLoggedIn(req, res);
+/////
+
+router.get("/", async (req, res) => {
+    if (await toolbox.isAuthenticated(req)) {
+        res.redirect("/profile");
     } else {
         res.render("login");
     }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    if (typeof req.session.session === "string") {
-        checkLoggedIn(req, res);
+    if (await toolbox.isAuthenticated(req)) {
+        res.redirect("/profile");
     } else {
         login(req, res, username, password);
     }
