11fa1ad1011430dfbd48a45d77264ba2d2aa31c4
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2021-05-15 01:57:18 +0300

    Implemented storage of user data in session

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 // Requirements
 
+const User    = require("../classes/user");
 const toolbox = require("../toolbox");
 const express = require("express");
 const router  = express.Router();
@@ -7,26 +8,38 @@ const axios   = require("axios").default;
 
 /////
 
-function login(req, res, username, password) {
-    axios.post(`${toolbox.BASE_URL}/auth/login`, {
-        username: username,
-        password: password
-    }).then((response) => {
-        if (response.data.result === "ok") {
+async function login(req, res, username, password) {
+    console.log("Logging in ...");
+
+    try {
+        const response = await axios.post(`${toolbox.BASE_URL}/auth/login`, {
+            username: username,
+            password: password
+        });
+        
+        if (response.status === 200 && response.data.result === "ok") {
             req.session.session = response.data.token.session;
             req.session.refresh = response.data.token.refresh;
 
+            const userDetails = await toolbox.getAuthenticatedUserDetails(req);
+
+            req.session.user = new User(
+                userDetails.uuid,
+                userDetails.username
+            );
+
+            await req.session.user.refreshFollows(req);
+
             console.log("Logged In");
-            console.log()
 
             return res.redirect("/");
         }
         
         res.render("login", { error: "Can't login" });
-    }).catch((err) => {
-        console.error(err);
+    } catch (error) {
+        console.error(`An error occurred while logging in : ${error}`);
         res.render("login", { error: "An error occurred" });
-    });
+    }
 }
 
 /////
