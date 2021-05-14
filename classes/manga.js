a11f8676fecffc2720e158c23737cc8f5f91c395
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2021-05-15 01:55:08 +0300

    Created Manga class

---
+++ classes/manga.js
@@ -0,0 +1,25 @@
+class Manga {
+    constructor(uuid, title, altTitles, description, isLocked, originalLanguage, lastVolume, lastChapter, publicationDemographic, status, year, contentRating, tags, version, createdAt, updatedAt, author = {}, group = {}, readingStatus = "") {
+        this.uuid                   = (typeof uuid === "string") ? uuid : "";
+        this.title                  = (typeof title === "object") ? title : "";
+        this.altTitles              = (typeof altTitles === "object") ? altTitles : {};
+        this.description            = (typeof description === "object") ? description : {};
+        this.isLocked               = (typeof isLocked === "boolean") ? isLocked : true;
+        this.originalLanguage       = (typeof originalLanguage === "string") ? originalLanguage : "";
+        this.lastVolume             = (typeof lastVolume === "string") ? lastVolume : "";
+        this.lastChapter            = (typeof lastChapter === "string") ? lastChapter : "";
+        this.publicationDemographic = (typeof publicationDemographic === "string") ? publicationDemographic : "";
+        this.status                 = (typeof status === "string") ? status : "";
+        this.year                   = (typeof year === "number") ? year : 0;
+        this.contentRating          = (typeof contentRating === "string") ? contentRating : "";
+        this.tags                   = (typeof tags === "object") ? tags : [];
+        this.version                = (typeof version === "number") ? version : 1;
+        this.createdAt              = (typeof createdAt === "string") ? createdAt : "";
+        this.updatedAt              = (typeof updatedAt === "string") ? updatedAt : "";
+        this.author                 = (typeof author === "object") ? author : {};
+        this.group                  = (typeof group === "object") ? group : {};
+        this.readingStatus          = (typeof readingStatus === "string") ? readingStatus : "";
+    }
+}
+
+module.exports = Manga;
